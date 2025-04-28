6620d88d09951c90b9bd0b0bc28b6a461fd11da9
vds89 <vincenzodesimone04@gmail.com>
2025-04-28 23:04:35 +0200

    Analytics page, Custome date range picker_v2

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -60,12 +60,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        
       }}
       {...props}
     />
